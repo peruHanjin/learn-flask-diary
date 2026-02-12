f47b09bc7d85c7b000be154e072d816230e483cf
peruHanjin <hanjin017269@gmail.com>
2026-02-12 13:20:16 +0900

    02 첫 앱(App) 생성

--- app.py
+++ app.py
@@ -1,17 +1,6 @@
-from flask import Flask, render_template
+from Diary import create_app
 
-# 웹 서버 역할 Flask APP 생성
-app = Flask(__name__)
+app = create_app()
 
-# 라우팅 설정 - url을 통한 접속 > 응답을 담당
-@app.route('/')
-def index():
-    return render_template('index.html')
-
-@app.route('/page')
-def page():
-    return render_template('page.html')
-
-# Flask 앱 가동(run)
 if __name__ == "__main__":
     app.run(debug=True)
