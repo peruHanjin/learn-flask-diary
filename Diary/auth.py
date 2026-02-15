1c8599fa491e31fcddfa5ad32b143e25ac9e5ec3
peruHanjin <hanjin017269@gmail.com>
2026-02-15 23:35:18 +0900

    04 Template과 Jinja

--- Diary/auth.py
+++ Diary/auth.py
@@ -1,15 +1,18 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
 
+# Creat Blueprint
 auth = Blueprint('auth', __name__)
 
+# route
 @auth.route('/sign-up')
 def sign_up():
-    return '<h1>sign-up<h1>'
+    return render_template('sign_up.html')
 
 @auth.route('/logout')
 def logout():
-    return '<h1>logout<h1>'
+    return render_template('logout.html')
 
+# login
 @auth.route('/sign-in')
 def sign_in():
-    return '<h1>sign-in<h1>'
+    return render_template('sign_in.html', user = "SemiCircle")
