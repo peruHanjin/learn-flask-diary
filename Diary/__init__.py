8847bed3f5f154f520aa6280e817542dba617b67
peruHanjin <hanjin017269@gmail.com>
2026-02-22 19:53:04 +0900

    08 DB연결 준비, SqlAlchemy

--- Diary/__init__.py
+++ Diary/__init__.py
@@ -1,9 +1,16 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+# Creat DB Connection
+db = SQLAlchemy()
+DB_NAME = "database.db"
 
 def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = 'semicircle_secret_key'
-    
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
+    db.init_app(app)
+
     #Import Blueprint
     from .views import views
     from .auth import auth
