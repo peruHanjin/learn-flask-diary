71f35bfdbc2cf56ac245e48307143cba7ea7f6f5
peruHanjin <hanjin017269@gmail.com>
2026-02-25 09:38:41 +0900

    10 외래키(참조기, ForeignKey)

--- Diary/models.py
+++ Diary/models.py
@@ -10,7 +10,7 @@ class User(db.Model, UserMixin):
     email = db.Column(db.String(120), unique=True, nullable=False)
     nickname = db.Column(db.String(80), unique=True, nullable=False)
     password = db.Column(db.String(200))
-
+    notes = db.relationship('Note')
 
 # define Note Model
 class Note(db.Model):
@@ -18,3 +18,4 @@ class Note(db.Model):
     title = db.Column(db.String(50))
     content = db.Column(db.String(2000))
     datetime = db.Column(db.DateTime, default=datetime.now(), onupdate=datetime.now())
+    user_id = db.Column(db.Integer, db.Foreignkey('user.id'))
