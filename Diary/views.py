1a0176b85d3a5a8cfbee3d84ed6dfe2677fd8537
peruHanjin <hanjin017269@gmail.com>
2026-02-28 11:08:31 +0900

    16 메모장 HTML, 17 메모 생성 기능

--- Diary/views.py
+++ Diary/views.py
@@ -1,9 +1,34 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, flash, redirect, url_for
 from flask_login import login_required, current_user
+from .models import Note
+from .import db
+
 
 views = Blueprint('views', __name__)
 
-@views.route('/')
+# route
+@views.route('/', methods = ['GET','POST'])
 @login_required
 def home():
+    # POST : 메모 생성
+    if request.method == "POST":
+        title = request.form.get('note-title')
+        content = request.form.get('note-content')
+
+        # 유효성 검사
+        if len(title) < 1 or len(content) < 1:
+            flash("제목 또는 내용이 없습니다.", category = "error")
+        elif len(title) > 50:
+            flash("제목이 너무 깁니다. 50자 이내", category = "error")
+        elif len(content) > 2000:
+            flash("내용이 너무 깁니다. 2000자 이내", category="error")
+        else :
+            # note 인스턴스 생성 -> DB에 저장
+            new_note = Note(title=title, content=content, user_id=current_user.id)
+            db.session.add(new_note)
+            db.session.commit()
+
+            flash("메모 생성 완료", category="success")
+            return redirect(url_for('views.home')) # 없으면 메모 계속 생성
+
     return render_template('home.html')
