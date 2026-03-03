adb626fc72196fb35642c7f07c363672207cc521
peruHanjin <hanjin017269@gmail.com>
2026-03-03 23:37:33 +0900

    17 메모 생성 기능

--- Diary/static/index.js
+++ Diary/static/index.js
@@ -1,1 +1,19 @@
-console.log("test")
+// 기본적인 함수 생성 방법
+const deleteNote = function (noteId){console.log(`삭제할 메모 id ${noteId}`);
+
+    // 전달할 데이터
+    let note = {
+        noteId : noteId
+    }
+    // 삭제 ajax
+    fetch('/delete-note',{
+        method : 'POST',
+        body : JSON.stringify(note),
+        headers: {
+            "Content-Type": "application/json"
+        },
+    }).then((response) => response.json())
+    .then(()=>{
+        window.location.href = '/'; // 새로고침
+    });
+}
--- Diary/views.py
+++ Diary/views.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, request, flash, redirect, url_for
+from flask import Blueprint, render_template, request, flash, redirect, url_for, jsonify
 from flask_login import login_required, current_user
 from .models import Note
 from .import db
@@ -32,3 +32,19 @@ def home():
             return redirect(url_for('views.home')) # 없으면 메모 계속 생성
 
     return render_template('home.html')
+
+# 메모 삭제 기능
+@views.route('/delete-note', methods=['POST'])
+def delete_note():
+    # POST : 메모 삭제
+    if request.method == "POST":
+        note = request.get_json()
+        note_id = note.get('noteId')
+
+        select_note = Note.query.get(note_id)
+        if select_note:
+            if select_note.user_id == current_user.id : 
+                db.session.delete(select_note)
+                db.session.commit()
+
+        return jsonify({})
